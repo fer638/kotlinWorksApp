2345abb28cb5c6ff26f7cd211bb6b570a74869cc
fer638 <jorgefer638@gmail.com>
2023-06-13 13:38:23 -0600

    subiendo repositorio

---
+++ app/src/main/java/com/jorgefer/kotlinworks/apActivity.kt
@@ -0,0 +1,11 @@
+package com.jorgefer.kotlinworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class apActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ap)
+    }
+}
